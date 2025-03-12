ed1fda8495323c3adddfb75f3150bdd21b37130e
jibon49 <imoinul749@gmail.com>
2025-03-12 10:34:14 +0600

    first commit

---
+++ src/layout/Main.jsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom"
+
+
+function Main() {
+  return (
+    <div>
+        <Outlet>
+            
+        </Outlet>
+    </div>
+  )
+}
+
+export default Main
